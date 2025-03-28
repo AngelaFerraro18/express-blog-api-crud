37a5e8473f20cbdc2a954ab6684e387a7c6dc5a5
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-03-28 14:21:24 +0100

    feat: import router and add app.use

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 //importo express
 const express = require('express');
+//importo routers
+const postsRouters = require('./routers/postsRouters.js');
 const app = express();
 const port = 3000;
+
+//bacheca con i post
+app.use('/posts', postsRouters);
 
 app.listen(port, () =>{
     console.log(`Il server è in ascolto alla porta: ${port}.`);
